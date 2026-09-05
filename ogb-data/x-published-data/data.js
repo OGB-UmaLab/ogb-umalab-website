e8bcb5de7db247dc77cd673b21de09c6c055cb45
OGB-UmaLab <pinopi0404@gmail.com>
2026-09-05 11:00:56 +0900

    Add September 5 X published data

--- ogb-data/x-published-data/data.js
+++ ogb-data/x-published-data/data.js
@@ -1,3 +1,4 @@
 window.ogbXPublishedData = [
+  { date: "2026-09-05", races: "京成杯オータムハンデキャップ・札幌2歳ステークス", url: "https://x.com/gichu_omoto/status/2096041493538754896" },
   // { date: "YYYY-MM-DD", races: "対象レース名", url: "" },
 ];
