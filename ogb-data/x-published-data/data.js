1b9af18b22926a317bffe7793b99b3f54ddec758
OGB-UmaLab <pinopi0404@gmail.com>
2026-09-20 09:09:18 +0900

    Add Sep 20 All Comers OGB published data

--- ogb-data/x-published-data/data.js
+++ ogb-data/x-published-data/data.js
@@ -1,4 +1,5 @@
 window.ogbXPublishedData = [
+  { date: "2026-09-20", races: "オールカマー", url: "https://note.com/gichu_omoto/n/n575e0dbef61d" },
   { date: "2026-09-13", races: "ローズステークス・セントライト記念", url: "https://note.com/gichu_omoto/n/n9c33f0a60495" },
   { date: "2026-09-12", races: "チャレンジカップ", url: "https://note.com/gichu_omoto/n/n3ca531401856" },
   { date: "2026-09-06", races: "紫苑ステークス・セントウルステークス", url: "https://x.com/gichu_omoto/status/2096410705734091097" },
